1b7f8a42c4792492d00b65bbe88f58a2933659ca
codesleepcode <81725710+codesleepcode@users.noreply.github.com>
2021-06-25 12:30:48 +1000

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #give permission for everything in the express-app directory
-sudo chmod -R 777 /home/ec2-user/express-app
+sudo chmod -R 777 /home/ec2-user/exp-app
 
 #navigate into our working directory where we have all our github files
-cd /home/ec2-user/express-app
+cd /home/ec2-user/exp-app
 
 #add npm and node to path
 export NVM_DIR="$HOME/.nvm"	
